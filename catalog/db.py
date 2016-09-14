427ac9e8bb626519753cae396b5f9352b3067770
Leilani Ann Raranga <Retrosole@gmail.com>
2016-09-13 17:09:39 -0700

    Update db.py

--- catalog/db.py
+++ catalog/db.py
@@ -67,7 +67,7 @@ class Item(Base):
 
 
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('postgresql://catalog:secure_password@localhost/catalog')
  
 
 Base.metadata.create_all(engine)
