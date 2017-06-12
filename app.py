b185e821bbc1fb7b3812449e6cd4686527c2e412
leilani <leilani.ann.raranga@gmail.com>
2017-06-11 17:19:24 -0700

    Herokue expects port variable

--- app.py
+++ app.py
@@ -334,6 +334,6 @@ def getUserID(email):
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
